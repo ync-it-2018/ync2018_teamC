b4f605ef1ab65665957f355be85e5f854dd6c78d
team_c <team_c@791f864d-2a39-41cb-9c68-7b0e3d11f3c2>
2018-10-15 09:08:56 +0000

    #배세희 html to jsp

--- src/main/java/kr/ync/project/controller/CommonExceptionAdvice.java
+++ src/main/java/kr/ync/project/controller/CommonExceptionAdvice.java
@@ -4,18 +4,27 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
-// 전역 오류 처리
+/**
+ * CommonExceptionAdvice.java
+ * 
+ * @Author	: 배세희
+ * @Date 	: 2018. 10. 15.
+ * @Description
+ * 공통 error 처리용 controller 
+ *
+ */
 @ControllerAdvice
 public class CommonExceptionAdvice {
 
-	/*@ExceptionHandler(Exception.class)
-	public String common(Exception e) {
 
-		log.info(e.toString());
-
-		return "error_common";
-	}*/
-
+	/**
+	 * @Description
+	 *
+	 * @Method Name : errorModelAndView
+	 * @param ex
+	 * @return
+	 *
+	 */
 	@ExceptionHandler(Exception.class)
 	private ModelAndView errorModelAndView(Exception ex) {
 		
