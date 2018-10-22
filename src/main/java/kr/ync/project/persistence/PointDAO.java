4177e5aecf01f32121c10910ffdc6b3958eff0a3
team_c <team_c@791f864d-2a39-41cb-9c68-7b0e3d11f3c2>
2018-10-22 01:31:03 +0000

    #전유진 게시판 글 등록

---
+++ src/main/java/kr/ync/project/persistence/PointDAO.java
@@ -0,0 +1,8 @@
+package kr.ync.project.persistence;
+
+public interface PointDAO {
+
+	public void updatePoint(String uid,int point)throws Exception;
+	
+}
+
---
+++ src/main/java/kr/ync/project/service/PointDAO.java
@@ -0,0 +1,8 @@
+package kr.ync.project.service;
+
+public interface PointDAO {
+
+	public void updatePoint(String uid,int point)throws Exception;
+	
+}
+
