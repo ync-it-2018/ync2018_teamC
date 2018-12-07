bc2641486ccc35ec451ba908dfab9c296a8d8ae7
team_c <team_c@791f864d-2a39-41cb-9c68-7b0e3d11f3c2>
2018-12-07 12:13:24 +0000

    #김진희 1207 회원가입, productup수정

--- branches/teamC_project_DEV-1.0/src/main/java/kr/ync/project/service/ProductService.java
+++ branches/teamC_project_DEV-1.0/src/main/java/kr/ync/project/service/ProductService.java
@@ -8,9 +8,11 @@ public interface ProductService {
 	//상품 목록
 	public List<ProductVO> listProduct();
 	//상품 상세
-	public ProductVO detailProduct(int p_idx);
+	public ProductVO detailProduct(String p_code);
 	//상품수정
 	public void updateProduct(ProductVO vo);
 	//상품삭제
-	public void deleteProduct(int p_idx);
+	public void deleteProduct(String p_code);
+	
+	public void insertProduct(ProductVO vo);
 }
